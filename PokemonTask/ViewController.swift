60261abe1ce5906eb6425f7ee461236402c5a29c
kyurorong <zzikcomputer@gmail.com>
2024-07-17 17:05:26 +0900

    Initial Commit

---
+++ PokemonTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonTask
+//
+//  Created by 김인규 on 7/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
